eef1a1381bfba970c7c899361f41d7689d6def85
ATHUL0527 <www.athulappu0527@gmail.com>
2024-10-19 10:13:43 +0530

    view engine set up

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express=require("express");
+const path=require("path")
 const app=express();
 const env=require("dotenv").config();
 const db=require("./config/db");
+const userRouter=require("./routes/userRouter")
 db()
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/admin")]);
+app.use(express.static("public"));
+
+app.use("/",userRouter);
 
 
 app.listen(process.env.PORT,()=>{
